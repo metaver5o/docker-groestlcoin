64d9f592cde947e318fc56f843903bcd5b6e2909
gruve-p <gruve-p@users.noreply.github.com>
2021-10-04 03:41:24 +0200

    Refactor Groestlcoin Core and add 22.0 (#35)
    * Unify/refactor Bitcoin Core and Groestlcoin Core
    
    * Untouch Bitcoin Core
    
    * Add Groestlcoin Core 22.0

--- groestlcoin/2.21.1/docker-entrypoint.sh
+++ groestlcoin/2.21.1/docker-entrypoint.sh
@@ -4,7 +4,32 @@ set -e
 if [[ "$1" == "groestlcoin-cli" || "$1" == "groestlcoin-tx" || "$1" == "groestlcoind" || "$1" == "test_groestlcoin" ]]; then
 	mkdir -p "$GROESTLCOIN_DATA"
 
+	CONFIG_PREFIX=""
+	if [[ "${GROESTLCOIN_NETWORK}" == "regtest" ]]; then
+		CONFIG_PREFIX=$'regtest=1\n[regtest]'
+	elif [[ "${GROESTLCOIN_NETWORK}" == "testnet" ]]; then
+		CONFIG_PREFIX=$'testnet=1\n[test]'
+	elif [[ "${GROESTLCOIN_NETWORK}" == "mainnet" ]]; then
+		CONFIG_PREFIX=$'mainnet=1\n[main]'
+	else
+		GROESTLCOIN_NETWORK=""
+	fi
+
+	if [[ "$GROESTLCOIN_WALLETDIR" ]] && [[ "$GROESTLCOIN_NETWORK" ]]; then
+		NL=$'\n'
+		WALLETDIR="$GROESTLCOIN_WALLETDIR/${GROESTLCOIN_NETWORK}"
+		WALLETFILE="${WALLETDIR}/wallet.dat"
+		mkdir -p "$WALLETDIR"
+		chown -R groestlcoin:groestlcoin "$WALLETDIR"
+		CONFIG_PREFIX="${CONFIG_PREFIX}${NL}walletdir=${WALLETDIR}${NL}"
+		if ! [[ -f "${WALLETFILE}" ]]; then
+		  echo "The wallet does not exists, creating it at ${WALLETDIR}..."
+		  gosu groestlcoin groestlcoin-wallet "-datadir=${WALLETDIR}" "-wallet=" create
+		fi
+	fi
+
 	cat <<-EOF > "$GROESTLCOIN_DATA/groestlcoin.conf"
+	${CONFIG_PREFIX}
 	printtoconsole=1
 	rpcallowip=::/0
 	rpcbind=0.0.0.0:1441
